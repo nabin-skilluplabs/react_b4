6df443a55580c4126fd6d9ca37f6331145dc6869
Nabin Singh <nabin@skilluplabs.com.au>
2025-01-28 20:27:49 +1100

    Created a new component

--- react_quick_start/src/App.jsx
+++ react_quick_start/src/App.jsx
@@ -1,5 +1,20 @@
+import Profile from './components/Profile';
+
 function App() {
-  return <></>;
+  return (
+    <div className="flex justify-center p-4">
+      <article className="bg-white p-4 w-1/2 rounded-2xl shadow-xl">
+        <h2 className="text-3xl font-bold mb-2">- Amazing scientists - </h2>
+        <div className="flex gap-1">
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+        </div>
+      </article>
+    </div>
+  );
 }
 
 export default App;
---
+++ react_quick_start/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      width={100}
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default Profile;
