4bcc1f66dd5c88de7f255f1830e27e9e2a5b183f
Nabin Singh <nabin@skilluplabs.com.au>
2025-01-29 19:20:21 +1100

    Add profile

--- react_quick_start/src/App.jsx
+++ react_quick_start/src/App.jsx
@@ -1,10 +1,16 @@
-import Profile from './components/Profile';
+import { Profile } from './components/Profile';
 
 function App() {
+  const greetings = 'Hello!';
   return (
     <div className="flex justify-center p-4">
       <article className="bg-white p-4 w-1/2 rounded-2xl shadow-xl">
-        <h2 className="text-3xl font-bold mb-2">- Amazing scientists - </h2>
+        <h2
+          className="text-3xl font-bold mb-2"
+          style={{ color: 'red', fontStyle: 'italic' }}
+        >
+          - {greetings}, Amazing scientists -{' '}
+        </h2>
         <div className="flex gap-1">
           <Profile />
           <Profile />
--- react_quick_start/src/components/Profile.jsx
+++ react_quick_start/src/components/Profile.jsx
@@ -1,11 +1,11 @@
-function Profile() {
+export function Profile() {
+  const name = 'Dr. Katherine Johnson';
   return (
     <img
       src="https://i.imgur.com/MK3eW3Am.jpg"
       width={100}
       alt="Katherine Johnson"
+      title={name}
     />
   );
 }
-
-export default Profile;
